e01ad5768289e0abb2d4795d340e63ba60569ca3
Alexander Ljungberg <aljungberg@slevenbits.com>
2011-05-24 11:41:45 -0400

    Use the Apple like "WireLoad Inc." form instead of "WireLoad, Inc."

--- www/wp-content/themes/wireload-02/footer-bare.php
+++ www/wp-content/themes/wireload-02/footer-bare.php
@@ -2,7 +2,7 @@
 
 	<div id="footer">
 		<div id="foot">
-			<div id="copy">&copy; <strong>2006-2011.</strong> WireLoad, Inc.</div>
+			<div id="copy">&copy; <strong>2006-2011.</strong> WireLoad Inc.</div>
 			<a href="http://wireload.net" id="bot-logo"><img src="<?php echo_images_uri() ?>/bot_logo.png" alt="Wireload" /></a>
 		</div>
 	</div>
