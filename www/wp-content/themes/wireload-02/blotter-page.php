61f4a9fb7c6db13c8bb3d97b3d06a1f7cc135bf1
Alexander Ljungberg <aljungberg@slevenbits.com>
2011-05-23 21:44:53 -0400

    Make Blotter page more cohesive and better looking.

--- www/wp-content/themes/wireload-02/blotter-page.php
+++ www/wp-content/themes/wireload-02/blotter-page.php
@@ -15,7 +15,18 @@ Template Name: Blotter Page
 	<h2 id="prod-name"><img src="<?php echo_images_uri() ?>/blotter/blotter_ico.png" alt="Blotter Icon" />The future will never look the same again.</h2>
 
 	<div id="below-promo">
-		<div class="container">
+		<div class="container top">
+  		<div class="description">
+  		  <span class="left">
+  			<p>Did you ever mark something in your calendar only to <strong>forget to check the calendar</strong>?</p>
+  			<p>No more. Blotter is a Mac application which places your weekly <strong>calendar right on your desktop</strong>. Discreet and out of the way, yet only a glance away.</p>
+  			</span>
+  			<span class="right">
+  			<p>With your week right before you, it is fast to see if you can make a meeting on Tuesday or if you’ll be able to go home early on Friday.</p>
+  			<p>Blotter is fully compatible with Mac OS X Snow Leopard, including Spaces, Exposé and Mobile Me.</p>
+  			</span?
+  		</div>
+
 			<a href="http://itunes.apple.com/us/app/blotter/id406580224?mt=12" class="app-store-button"><img src="<?php echo_images_uri() ?>/app_store.png" alt="Available Now On The App Store" /></a>
 			<div class="like-it">
 			  <div>
@@ -24,25 +35,21 @@ Template Name: Blotter Page
         </div>
     	</div>
 
-			<div class="description">
-				<p>Did you ever mark something in your calendar only to <strong>forget to check the calendar</strong>?</p>
-				<p>No more. Blotter is a Mac application which places your weekly <strong>calendar right on your desktop</strong>. Discreet and out of the way, yet only a glance away.</p>
-			</div>
+  		<ul class="item-list">
+  			<li>
+  				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/stop-icon.png" alt="icon" width="37" height="38" /></div>
+  				<p><strong>Ready to go.</strong> Blotter connects directly to your regular iCal calendar. No setup required.</p>
+  			</li>
+  			<li>
+  				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/megaphone-icon.png" alt="icon" width="39" height="38" /></div>
+  				<p><strong>What’s up?</strong> The “Right Now” view highlights events as they come.</p>
+  			</li>
+  			<li>
+  				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/checkmark-icon.png" alt="icon" width="38" height="38" /></div>
+  				<p><strong>To do list, too.</strong> Blotter will display the next few items from your iCal tasks list as well.</p>
+  			</li>
+  		</ul>
 		</div>
-		<ul class="item-list">
-			<li>
-				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/stop-icon.png" alt="icon" width="37" height="38" /></div>
-				<p><strong>Ready to go.</strong> Blotter connects directly to your regular iCal calendar. No setup required.</p>
-			</li>
-			<li>
-				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/megaphone-icon.png" alt="icon" width="39" height="38" /></div>
-				<p><strong>What’s up?</strong> The “Right Now” view highlights events as they come.</p>
-			</li>
-			<li>
-				<div class="alignleft"><img src="<?php echo_images_uri() ?>/blotter/checkmark-icon.png" alt="icon" width="38" height="38" /></div>
-				<p><strong>To do list, too.</strong> Blotter will display the next few items from your iCal tasks list as well.</p>
-			</li>
-		</ul>
 		<div class="gallery-box">
 			<div class="heading">
 				<strong>See it for yourself.</strong>
@@ -78,18 +85,26 @@ Template Name: Blotter Page
 				</ul>
 			</div>
 		</div>
-		<div class="container">
-			<div class="list-holder">
-				<div class="list-frame">
-					<ul class="advantage-list">
-						<li class="type1"><strong>Less than meets the eye.</strong> Blotter uses very little memory and processing power.</li>
-						<li class="type2"><strong>Rolling 7 day option.</strong> View the 7 next days instead of a regular week.</li>
-						<li class="type3"><strong>Narrow option.</strong> Save space with only a to do list and upcoming events.</li>
-					</ul>
-				</div>
-			</div>
-			<p>Blotter is an easy way to increase your productivity. With your week right before you, it is fast to see if you can make a meeting on Tuesday or if you’ll be able to go home early on Friday.</p>
-			<p>Blotter is fully compatible with Mac OS X Snow Leopard, including Spaces, Exposé and Mobile Me.</p>
+		<div class="container bottom">
+      <div class="testimonials">
+        <h2>What People Say</h2>
+  			<span class="left">
+    			<p>Pick of the Week in <a href="http://wiki.twit.tv/wiki/MacBreak_Weekly_239">MacBreak Weekly</a>!</p>
+    			<p><em>"Blotter is a great productivity application. It places your iCal calendar directly onto your desktop in a discreet and out of the way fashion, while also being easily accessible. No setup is required, all you have to do is install it from the Mac App Store and your ready to go."</em><br/>- <a href="http://macappolis.com/index.php?id=2973847242085471834">MacAppolis</a></p>
+    			<p><em>"It's pretty...I like [it]."</em><br/> - Leo Leporte, MacBreak Weekly</p>
+  			</span>
+  			<span class="right">
+  			  <p><em>"@blotterapp is the only app that i USE RELIGIOUSLY ! its brilliant !"</em><br/> - @drbuk</p>
+          <p><em>"[Blotter is] well designed."</em><br/> - Alex Lindsay, MacBreak Weekly</p>
+    			<p><em>"With Blotter, you can display your iCal content on your desktop and so keep an eye on your important stuff much easier – and surprisingly enough, find that there just might be time to do everything properly."</em><br/> - Julia Altermann, <a href="http://mac.appstorm.net/reviews/productivity-review/blotter-displaying-ical-on-your-desktop/">Mac App Storm</a></p>
+  			</span>
+      </div>
+			<ul class="advantage-list">
+				<li class="type1"><strong>Less than meets the eye.</strong> Blotter uses very little memory and processing power.</li>
+				<li class="type2"><strong>Rolling 7 day option.</strong> View the 7 next days instead of a regular week.</li>
+				<li class="type3"><strong>Narrow option.</strong> Save space with only a to do list and upcoming events.</li>
+			</ul>
+
 			<a href="http://itunes.apple.com/us/app/blotter/id406580224?mt=12" class="app-store-button"><img src="<?php echo_images_uri() ?>/app_store.png" alt="Available Now On The App Store" /></a>
 		</div>
 	</div>
