6d56240cf690ac1ffca09fbb724c5d818bf5a225
Viktor Petersson <vpetersson@wireload.net>
2011-07-13 22:34:53 +0200

    Changed relative image links to point to static.wireload.net

--- www/wp-content/themes/wireload-02/home-page.php
+++ www/wp-content/themes/wireload-02/home-page.php
@@ -16,19 +16,19 @@ Template Name: Home Page
 	<div id="s-wrap">
 		<div class="item cur" id="sw-blotter">
 		  <a href='/products/blotter/'>
-			<img src="images/blotter_pic.png" alt="Blotter - The future will never look the same." style="bottom: 0px; left: 5px;" />
+			<img src="http://static.wireload.net/images/blotter_pic.png" alt="Blotter - The future will never look the same." style="bottom: 0px; left: 5px;" />
 			</a>
 		</div>
 		<div class="item" id="si2">
 		  <a href="/products/quiet/">
-			<img src="images/Quiet_slogan.png" alt="Preserve your train of thought and be effective with Quiet." style="bottom: 137px; left: 66px;" />
-			<img src="images/Quiet_pic.png" alt="Quiet" style="bottom: 46px; left: 195px;" />
+			<img src="http://static.wireload.net/images/Quiet_slogan.png" alt="Preserve your train of thought and be effective with Quiet." style="bottom: 137px; left: 66px;" />
+			<img src="http://static.wireload.net/images/Quiet_pic.png" alt="Quiet" style="bottom: 46px; left: 195px;" />
 			<p class="s-right" style="top: 276px; right: 64px;">Preserve your train<br />of thought and be effective<br />with <strong>Quiet.</strong></p>
 		  </a>
 		</div>
 		<div class="item">
 		  <a href="http://www.yippiemove.com">
-			<img src="images/YippieMove_pic.png" alt="YippieMove - Fast and easy movement of email." style="bottom: 0px; left: -60px;" />
+			<img src="http://static.wireload.net/images/YippieMove_pic.png" alt="YippieMove - Fast and easy movement of email." style="bottom: 0px; left: -60px;" />
 			</a>
 		</div>
 	</div>
@@ -43,7 +43,7 @@ Template Name: Home Page
 	<div class="item">
 		<a href="/products/blotter/">
 			<span class="img">
-				<img src="images/img1.png" alt="Blotter" />
+				<img src="http://static.wireload.net/images/img1.png" alt="Blotter" />
 				<span class="i-border"></span>
 			</span>
 		</a>
@@ -54,7 +54,7 @@ Template Name: Home Page
 	<div class="item">
 		<a href="/products/quiet/">
 			<span class="img">
-				<img src="images/img2.png" alt="Quiet" />
+				<img src="http://static.wireload.net/images/img2.png" alt="Quiet" />
 				<span class="i-border"></span>
 			</span>
 		</a>
@@ -65,7 +65,7 @@ Template Name: Home Page
 	<div class="item">
 		<a href="http://www.yippiemove.com">
 			<span class="img">
-				<img src="images/img3.png" alt="YippieMove" />
+				<img src="http://static.wireload.net/images/img3.png" alt="YippieMove" />
 				<span class="i-border"></span>
 			</span>
 		</a>
